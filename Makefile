65516723649eccfddfde0a1ff070662871ccea60
vinhtc27 <vinhtc27@gmail.com>
2023-12-01 14:41:28 +0700

    finish part 1 day 1

--- Makefile
+++ Makefile
@@ -11,7 +11,7 @@ run:
 	cargo run
 
 test:
-	cargo test --package aoc2023 --lib -- days::day01::tests::test_example_answers
+	make run TEST=1
 
 update:
 	rustup update && cargo outdated && cargo update && cargo upgrade && cargo audit
--- src/days/day01.rs
+++ src/days/day01.rs
@@ -1,51 +1,16 @@
 use crate::{DayResult, IntoDayResult};
 
-macro_rules! sort_arr {
-    ( $val:expr, $arr:expr ) => {
-        if $val > $arr[0] {
-            $arr[2] = $arr[1];
-            $arr[1] = $arr[0];
-            $arr[0] = $val;
-        } else if $val > $arr[1] {
-            $arr[2] = $arr[1];
-            $arr[1] = $val;
-        } else if $val > $arr[2] {
-            $arr[2] = $val;
-        }
-    };
-}
-
 pub fn run(input: &'static str, _: bool) -> anyhow::Result<DayResult> {
-    let mut calorie_sums = [0; 3];
+    let part1: u32 = input
+        .lines()
+        .map(|line| {
+            let first = line.chars().find(|c| c.is_numeric()).unwrap();
+            let last = line.chars().rev().find(|c| c.is_numeric()).unwrap();
+            format!("{}{}", first, last).parse::<u32>().unwrap()
+        })
+        .sum();
 
-    let input = input.as_bytes();
-    let mut curr_int: u32 = 0;
-    let mut sum = 0;
-    let mut last_newline = 0;
-
-    for (i, &byte) in input.iter().enumerate() {
-        match byte {
-            b'\n' => {
-                if last_newline + 1 == i {
-                    sort_arr!(sum, calorie_sums);
-                    sum = 0;
-                } else {
-                    sum += curr_int;
-                    curr_int = 0;
-                    last_newline = i;
-                }
-            }
-            b'0'..=b'9' => {
-                curr_int = curr_int * 10 + (byte - b'0') as u32;
-            }
-            _ => unreachable!(),
-        }
-    }
-
-    sort_arr!(sum, calorie_sums);
-
-    let part1 = calorie_sums[0];
-    let part2 = calorie_sums.iter().sum::<u32>();
+    let part2 = 0;
 
     (part1, part2).into_result()
 }
@@ -61,8 +26,8 @@ mod tests {
         assert_eq!(
             result.unwrap(),
             DayResult {
-                part1: Some(Answers::U32(24_000)),
-                part2: Some(Answers::U32(45_000)),
+                part1: Some(Answers::U32(142)),
+                part2: Some(Answers::U32(0)),
             }
         );
     }
@@ -73,7 +38,7 @@ mod tests {
         assert_eq!(
             result.unwrap(),
             DayResult {
-                part1: Some(Answers::U32(69_836)),
+                part1: Some(Answers::U32(54601)),
                 part2: Some(Answers::U32(207_968)),
             }
         );
